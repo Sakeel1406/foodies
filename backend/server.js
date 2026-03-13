3010b1c3f21f30909e26946701981bd4ff49b4e0
Sakeel1406 <144681263+Sakeel1406@users.noreply.github.com>
2026-03-14 05:12:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 
 // CORS CONFIGURATION
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  allowedHeaders: ["Content-Type","Authorization","token"]
+}));
 
 // DB Connection
 connectDB();
