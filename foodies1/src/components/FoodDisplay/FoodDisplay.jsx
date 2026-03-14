0ffc75477b69092c8ce5eb1183ae01a409108737
Sakeel1406 <144681263+Sakeel1406@users.noreply.github.com>
2026-03-14 14:12:10 +0530

    Update FoodDisplay.jsx

--- foodies1/src/components/FoodDisplay/FoodDisplay.jsx
+++ foodies1/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,66 +1,84 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../Context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
 const FoodDisplay = ({ category }) => {
-    const { food_list, url } = useContext(StoreContext);
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 20;
 
-    const filteredList = food_list.filter(item => 
+    const { food_list, url } = useContext(StoreContext)
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const itemsPerPage = 20
+
+    // Reset page when category changes
+    useEffect(() => {
+        setCurrentPage(1)
+    }, [category])
+
+    // Filter foods
+    const filteredList = (food_list || []).filter(item =>
         category === "All" || category === item.category
-    );
+    )
 
-    
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    
-    
-    const currentItems = filteredList.slice(indexOfFirstItem, indexOfLastItem);
+    // Pagination calculation
+    const indexOfLastItem = currentPage * itemsPerPage
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
 
-    
-    const totalPages = Math.ceil(filteredList.length / itemsPerPage);
+    const currentItems = filteredList.slice(indexOfFirstItem, indexOfLastItem)
+
+    const totalPages = Math.ceil(filteredList.length / itemsPerPage)
 
     return (
         <div className='food-display' id='food-display'>
+
             <h2>Top dishes near you</h2>
+
             <div className="food-display-list">
+
                 {currentItems.map((item) => (
+
                     <FoodItem
                         key={item._id}
                         id={item._id}
                         name={item.name}
                         description={item.description}
                         price={item.price}
-                        image={url + "/images/" + item.image}
-                        rating={item.rating} 
+                        image={item.image ? `${url}/images/${item.image}` : "/placeholder.png"}
+                        rating={item.rating || 0}
                     />
+
                 ))}
+
             </div>
 
-            
             {totalPages > 1 && (
+
                 <div className="pagination">
-                    <button 
-                        disabled={currentPage === 1} 
+
+                    <button
+                        disabled={currentPage === 1}
                         onClick={() => setCurrentPage(prev => prev - 1)}
                     >
                         Previous
                     </button>
-                    
-                    <span>Page {currentPage} - {totalPages}</span>
 
-                    <button 
-                        disabled={currentPage === totalPages} 
+                    <span>
+                        Page {currentPage} of {totalPages}
+                    </span>
+
+                    <button
+                        disabled={currentPage === totalPages}
                         onClick={() => setCurrentPage(prev => prev + 1)}
                     >
                         Next
                     </button>
+
                 </div>
+
             )}
+
         </div>
     )
 }
 
-export default FoodDisplay;
+export default FoodDisplay
